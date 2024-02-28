61e75891d29b20139916080fa475494dfafde4e6
duddn2012 <gowoo123@naver.com>
2024-02-28 17:00:51 +0900

    Updates

--- src/components/items_v2/v2.ts
+++
@@ -1,3 +0,0 @@
-export {Item as ItemV2} from "./Item";
-export {Items as ItemsV2} from "./Items";
-export {ItemLayout as ItemLayoutV2} from "./ItemLayout";
--- src/contexts/session.d.ts
+++
@@ -1,6 +0,0 @@
-type LoginUser = { id: number; name: string };
-type Cart = { id: number; name: string; price: number };
-type Session = {
-  loginUser: LoginUser | null;
-  cart: Cart[];
-};
